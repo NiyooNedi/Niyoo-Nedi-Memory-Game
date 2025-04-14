ac31903df539ec1ebd3028a4949826e19cd46f97
NiyooNedi <niyoo.nedi1@gmail.com>
2025-04-14 00:45:53 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,22 +1,150 @@
-//Add your global variables here
+let pattern = [2, 2, 4, 3, 2, 1, 2, 4];
+let progress = 0; 
+let gamePlaying = false;
+
+let tonePlaying = false;
+let volume = 0.5;
+
+const startButton = document.getElementById("startButton");
+const stopButton = document.getElementById("stopButton");
+
+const clueHoldTime = 1000;
+const cluePauseTime = 333;
+const nextClueWaitTime = 1000;
+
+let guessCounter = 0;
+
+
+function startGame() {
+  
+  progress = 0;
+  gamePlaying = true;
+
+  startButton.classList.add("hidden");
+  stopButton.classList.remove("hidden");
+  
+  playClueSequence();
+}
+
+function stopGame(){
+
+  gamePlaying = false;
+  startButton.classList.remove("hidden");
+  stopButton.classList.add("hidden");
+}
+
+function lightButton(btn){
+
+  if (btn == 1){
+
+    document.getElementById("firstButton").classList.add("lit");
+    
+  } else if (btn == 2){
+
+    document.getElementById("secButton").classList.add("lit");
+    
+  } else if (btn == 3){
+
+    document.getElementById("thirdButton").classList.add("lit");
+    
+  } else {
+
+    document.getElementById("fourthButton").classList.add("lit");
+  }
+  
+}
+
+function clearButton(btn){
+
+  if (btn == 1){
+
+    document.getElementById("firstButton").classList.remove("lit");
+
+  } else if (btn == 2){
+
+    document.getElementById("secButton").classList.remove("lit");
+    
+  } else if (btn == 3){
+
+    document.getElementById("thirdButton").classList.remove("lit");
+    
+  } else {
+
+    document.getElementById("fourthButton").classList.remove("lit");
+  }
+  
+}
+
+
+function playSingleClue(btn){
+
+  if (gamePlaying){
+    
+    lightButton(btn);
+    playTone(btn, clueHoldTime);
+    setTimeout(clearButton, clueHoldTime, btn);
+  }
+}
+
+
+function playClueSequence(){
+
+  guessCounter = 0;
+  
+  context.resume();
+  let delay = nextClueWaitTime;
+
+  for(let i = 0; i <= progress; i++){
+
+    console.log("play single clue: " + pattern[i] + " in " + delay + "ms");
+    setTimeout(playSingleClue, delay, pattern[i]);
+    delay += clueHoldTime;
+    delay += cluePauseTime;
+  }
+}
+
+function loseGame(){
+
+  stopGame();
+  alert("You've selected an incorrect button. Game over!");
+}
+
+function winGame(){
+
+  stopGame();
+  alert("You've won the game!");
+}
+
+function guess(btn){
+
+  console.log("user guessed: " + btn);
+
+  if(!gamePlaying){
+
+    return;
+  }
+
+  if (btn != pattern[guessCounter]){
+
+    loseGame();
+    
+  } else if (progress != guessCounter){
+
+    guessCounter++;
+    
+  } else if (progress != pattern.length - 1){
+
+    progress++;
+    playClueSequence();
+    
+  } else {
+
+    winGame();
+  }
+}
 
 
 
-
-
-// Add your functions here
-
-
-
-
-
-
-
-
-
-
-// Sound Synthesis Functions for Steps 6-8
-// You do not need to edit the below code
 const freqMap = {
   1: 261.6,
   2: 329.6,
